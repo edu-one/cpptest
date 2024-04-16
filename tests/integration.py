9d91a1ab89c5ab238b5ba2ca559a88c934983bf0
Denys Valchuk <dvalchuk@gmail.com>
2024-04-16 15:53:03 +0300

    initial template test

---
+++ tests/integration.py
@@ -0,0 +1,73 @@
+# Copyright (C) Denys Valchuk - All Rights Reserved
+# ZHZhbGNodWtAZ21haWwuY29tCg==
+
+import os
+import unittest
+from subprocess import run
+from shutil import rmtree
+
+
+class TestTemplate(unittest.TestCase):
+
+    @classmethod
+    def cwd(cls):
+        return os.path.dirname(os.path.abspath(__file__))
+
+    def setUp(self):
+        pid = os.getpid()
+        self.test_name = f"stubname-{pid}"
+        self.test_version = "2.7.1"
+        self.test_dir = os.path.join(TestTemplate.cwd(), "build", "tests", self.test_name)
+        # Create the test directory (remove if already exists)
+        os.makedirs(self.test_dir, exist_ok=True)
+
+    def tearDown(self):
+        rmtree(self.test_dir)
+
+    def test_crete_project(self):
+        command = ["conan", "new", "dv_cpptest", "-d", f"name={self.test_name}", "-d", f"version={self.test_version}"]
+        # Run the command in the test directory
+        run(command, cwd=self.test_dir)
+
+        # Expected file names
+        expected_files = [
+            "conanfile.py",
+            "CMakeLists.txt",
+            f"include/{self.test_name}.h",
+            f"src/{self.test_name}.cpp",
+            ".clang-format",
+            ".gitignore",
+            ".cmake-format",
+            ".conanignore",
+            "LICENSE",
+            "README.md",
+            "requirements.txt",
+            "tests/CMakeLists.txt",
+            "tests/unit/CMakeLists.txt",
+            f"tests/unit/{self.test_name}_test.cpp",
+        ]
+
+        # Check if expected files exist
+        for filename in expected_files:
+            filepath = os.path.join(self.test_dir, filename)
+            self.assertTrue(os.path.exists(filepath), f"File {filename} not found")
+
+        # Check if the conanfile.py contains the correct name and version
+        conanfile_path = os.path.join(self.test_dir, "conanfile.py")
+        with open(conanfile_path, "r") as file:
+            content = file.read()
+            self.assertIn(f'name = \'{self.test_name}\'', content)
+            self.assertIn(f'version = \'{self.test_version}\'', content)
+
+        # Check if the CMakeLists.txt contains the correct name, version, file names, etc.
+        cmake_path = os.path.join(self.test_dir, "CMakeLists.txt")
+        with open(cmake_path, "r") as file:
+            content = file.read()
+            self.assertIn(f"project({self.test_name} VERSION {self.test_version} LANGUAGES CXX)", content)
+            self.assertIn(f"add_library({self.test_name} src/{self.test_name}.cpp)", content)
+            self.assertIn(f"target_include_directories({self.test_name} PUBLIC include)", content)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
